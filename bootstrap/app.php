b540404e6a8a6921abb055b975601c11608147b8
TumeloGeorge <tumeloge@gmail.com>
2026-07-09 22:43:21 +0200

    modified:   bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,17 @@ use App\Http\Middleware\AdminAuthenticate;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request as HttpRequest;
+
+HttpRequest::setTrustedProxies(
+    ['0.0.0.0/0'],
+    HttpRequest::HEADER_X_FORWARDED_FOR |
+    HttpRequest::HEADER_X_FORWARDED_HOST |
+    HttpRequest::HEADER_X_FORWARDED_PORT |
+    HttpRequest::HEADER_X_FORWARDED_PROTO |
+    HttpRequest::HEADER_X_FORWARDED_PREFIX |
+    HttpRequest::HEADER_X_FORWARDED_AWS_ELB,
+);
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
